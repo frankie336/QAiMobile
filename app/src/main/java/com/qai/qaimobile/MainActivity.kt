941cd1450c9f08345b6d4b3b86715bb6b11d07e1
primethanos <francis.neequaye@gmail.com>
2024-04-22 04:14:45 +0100

    Initial commit

---
+++ app/src/main/java/com/qai/qaimobile/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.qai.qaimobile
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.qai.qaimobile.ui.theme.QAiMobileTheme
+
+@Composable
+fun LoginScreen() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Login",
+            style = MaterialTheme.typography.headlineMedium,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+
+        OutlinedTextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Username") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp)
+        )
+
+        Button(
+            onClick = { /* Handle login button click */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text("Login")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun LoginScreenPreview() {
+    QAiMobileTheme {
+        LoginScreen()
+    }
+}
---
+++ app/src/main/java/com/qai/qaimobile/data/remote/LoginResponse.kt
@@ -0,0 +1,3 @@
+package com.qai.qaimobile.data.remote
+
+data class LoginResponse(val success: Boolean, val message: String, val token: String?)
